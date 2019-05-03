896ee226e00c65822ecc093510aa77bc5fd9bc29
Kevin Corbin <kecorbin@cisco.com>
2019-05-03 13:03:25 -0500

    initial tweaking of boilerplate python code

--- packages/vlan/python/vlan/main.py
+++ packages/vlan/python/vlan/main.py
@@ -3,42 +3,21 @@ import ncs
 from ncs.application import Service
 
 
-# ------------------------
-# SERVICE CALLBACK EXAMPLE
-# ------------------------
-class ServiceCallbacks(Service):
-
+class VLANService(Service):
+    """
+    This class represents the logic required to provision a VLAN
+    """
     # The create() callback is invoked inside NCS FASTMAP and
     # must always exist.
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
-        self.log.info('Service create(service=', service._path, ')')
+        self.log.info('Creating VLAN {} (id {})'.format(service._path,
+                                                        service.id))
 
         vars = ncs.template.Variables()
-        vars.add('DUMMY', '127.0.0.1')
+
         template = ncs.template.Template(service)
         template.apply('vlan-template', vars)
-
-    # The pre_modification() and post_modification() callbacks are optional,
-    # and are invoked outside FASTMAP. pre_modification() is invoked before
-    # create, update, or delete of the service, as indicated by the enum
-    # ncs_service_operation op parameter. Conversely
-    # post_modification() is invoked after create, update, or delete
-    # of the service. These functions can be useful e.g. for
-    # allocations that should be stored and existing also when the
-    # service instance is removed.
-
-    # @Service.pre_lock_create
-    # def cb_pre_lock_create(self, tctx, root, service, proplist):
-    #     self.log.info('Service plcreate(service=', service._path, ')')
-
-    # @Service.pre_modification
-    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-    # @Service.post_modification
-    # def cb_post_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
 
 
 # ---------------------------------------------
@@ -48,12 +27,11 @@ class Main(ncs.application.Application):
     def setup(self):
         # The application class sets up logging for us. It is accessible
         # through 'self.log' and is a ncs.log.Log instance.
-        self.log.info('Main RUNNING')
+        self.log.info('Launching VLAN Automation service')
 
         # Service callbacks require a registration for a 'service point',
-        # as specified in the corresponding data model.
-        #
-        self.register_service('vlan-servicepoint', ServiceCallbacks)
+        # as specified in the corresponding YANG model.
+        self.register_service('vlan-servicepoint', VLANService)
 
         # If we registered any callback(s) above, the Application class
         # took care of creating a daemon (related to the service/action point).
@@ -65,5 +43,4 @@ class Main(ncs.application.Application):
         # When the application is finished (which would happen if NCS went
         # down, packages were reloaded or some error occurred) this teardown
         # method will be called.
-
-        self.log.info('Main FINISHED')
+        self.log.info('Shutting down VLAN Automation service')
